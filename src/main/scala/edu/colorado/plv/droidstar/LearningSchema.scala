7b7464b5dbe887437bcc5ee0c8119ff203e740c0
octalsrc <nicklewchenko92@gmail.com>
2018-01-10 14:13:05 -0700

    Partial work on LearningSchema

--- src/main/scala/edu/colorado/plv/droidstar/LearningSchema.scala
+++ src/main/scala/edu/colorado/plv/droidstar/LearningSchema.scala
@@ -1,4 +1,32 @@
 package edu.colorado.plv.droidstar
 
-abstract class LearningSchema {
+abstract class LearningSchema(c: Context) {
+  private[this] var context: Context = c
+  private[this] var forOutput: Callback = null
+
+  def logl(m: String) = log("PURPOSE", m)
+
+  def logcb(output: String) = logl("Callback reported: " + output)
+
+  def respond(output: String) = {
+    if (forOutput != null) {
+      logcb(output);
+      forOutput.handleMessage(quickMessage(output));
+    } else {
+      throw new AssertionError(
+        "LP's callback was not initialized.  You must call reset() before you use it."
+      )
+    }
+  }
+
+  def reset(c: Callback): String {
+    forOutput = c
+    val flag: String = resetActions(context, c)
+    logl("LP has been reset.")
+    flag
+  }
+
+  def inputSet(): List[String] {
+    NotImplemented
+  }
 }
